cc4e2f8e87bfebd55712b8989e2823eb70d91ec0
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-09 15:09:20 +0200

    :tada: (init) Setup workshop project

---
+++ starterkit_plugin/includes/class-wpworkshop-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.digitalcopilote.io
+ * @since      1.0.0
+ *
+ * @package    Wpworkshop
+ * @subpackage Wpworkshop/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wpworkshop
+ * @subpackage Wpworkshop/includes
+ * @author     Digital Copilote <devs.digitalcopilote@gmail.com>
+ */
+class Wpworkshop_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ starterkit_plugin/includes/class-wpworkshop-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.digitalcopilote.io
+ * @since      1.0.0
+ *
+ * @package    Wpworkshop
+ * @subpackage Wpworkshop/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wpworkshop
+ * @subpackage Wpworkshop/includes
+ * @author     Digital Copilote <devs.digitalcopilote@gmail.com>
+ */
+class Wpworkshop_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
